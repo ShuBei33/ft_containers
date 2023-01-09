c9d44a37348415458875f88edb9c5c4086ec313e
Enora STOFFEL <estoffel@student.42.fr>
2023-01-10 00:46:36 +0100

    iter_traits, enable_if, is_integral ok

--- incl/utils.hpp
+++ incl/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: estoffel <estoffel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 16:34:19 by estoffel          #+#    #+#             */
-/*   Updated: 2023/01/07 20:07:35 by estoffel         ###   ########.fr       */
+/*   Updated: 2023/01/10 00:37:04 by estoffel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,15 +89,60 @@ namespace ft
 			}
 			return (first2!=last2);
 		}
+	//struct parent de std::is_integral
+	template <class T, T v> struct integral_constant {
+		static T value = v;
+		typedef T value_type;
+		typedef integral_constant<T, v> type;
+		operator T() { return v; }
+	};
+
+	typedef integral_constant<bool,false> false_type;
+	typedef integral_constant<bool,true> true_type;
 
 	//std::is_integral
-	template< class T > struct is_integral {
-		//public
-		typedef T value_type;
-	};
+	template< class T > struct is_integral : public false_type { };
+
+	template<> struct is_integral<bool> : public true_type { };
+	template<> struct is_integral<char> : public true_type { };
+	template<> struct is_integral<signed char> : public true_type { };
+	template<> struct is_integral<short int> : public true_type { };
+	template<> struct is_integral<int> : public true_type { };
+	template<> struct is_integral<long int> : public true_type { };
+	template<> struct is_integral<long long int> : public true_type { };
+	template<> struct is_integral<unsigned char> : public true_type { };
+	template<> struct is_integral<unsigned short int> : public true_type { };
+	template<> struct is_integral<unsigned int> : public true_type { };
+	template<> struct is_integral<unsigned long int> : public true_type { };
+	template<> struct is_integral<unsigned long long int> : public true_type { };
+	template<> struct is_integral<wchar_t> : public true_type { };
 
 	//std::iterator_traits
-	template <class Iterator> class iterator_traits {
-		
+	template <class Iter> class iterator_traits {
+		typedef typename Iter::difference_type difference_type;
+		typedef typename Iter::value_type value_type;
+		typedef typename Iter::pointer pointer;
+		typedef typename Iter::reference reference;
+		typedef typename Iter::iterator_category iterator_category;
 	};
+	
+	template <class T> class iterator_traits<T*> {
+		typedef std::ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+
+	template <class T> class iterator_traits<const T*> {
+		typedef std::ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef const T* pointer;
+		typedef const T& reference;
+		typedef std::random_access_iterator_tag iterator_category;
+	};
+
+	//std::enable_if
+	template<bool Cond, class T = void> struct enable_if {};
+	template<class T> struct enable_if<true, T> { typedef T type; };
 }
